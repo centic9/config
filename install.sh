5b9c61279c3fc453752bd46fe8a6ea834b78d14a
Dominik Stadler <dominik.stadler@gmx.at>
2015-06-25 22:00:40 +0200

    Correctly replace existing links

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+test -L ~/.bash_aliases && rm ~/.bash_aliases
 ln -sf `pwd`/.bash_aliases ~/
+
+test -L ~/.xinitrc && rm ~/.xinitrc
 ln -sf `pwd`/.xinitrc ~/
+
+test -L ~/.Xmodmap && rm ~/.Xmodmap
 ln -sf `pwd`/.Xmodmap ~/
