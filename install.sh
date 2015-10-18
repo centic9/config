6e29435e5355a5277cccc864857f065ee94bbb96
Dominik Stadler <dominik.stadler@gmx.at>
2015-10-18 21:42:12 +0200

    Add .gitconfig, make a backup of previous config-files

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-test -L ~/.bash_aliases && rm ~/.bash_aliases
+test -L ~/.bash_aliases && mv ~/.bash_aliases /tmp/.bash_aliases.sav
 ln -sf `pwd`/.bash_aliases ~/
 
-test -L ~/.xinitrc && rm ~/.xinitrc
+test -L ~/.xinitrc && mv ~/.xinitrc /tmp/.xinitrc.sav
 ln -sf `pwd`/.xinitrc ~/
 
-test -L ~/.Xmodmap && rm ~/.Xmodmap
+test -L ~/.Xmodmap && mv ~/.Xmodmap /tmp/.Xmodmap.sav
 ln -sf `pwd`/.Xmodmap ~/
 
-test -L ~/.toprc && rm ~/.toprc
+test -L ~/.toprc && mv ~/.toprc /tmp/.toprc.sav
 ln -sf `pwd`/.toprc ~/
+
+test -L ~/.gitconfig && mv ~/.gitconfig /tmp/.gitconfig.sav
+ln -sf `pwd`/.gitconfig ~/
